7956c852b8cc1bd1fdc1f92aacf6cd76cc2c0528
ViktorErmakov <79091602201@yandex.ru>
2024-09-26 23:32:10 +0300

    init: initial repo

---
+++ js30-api-hints/script.js
@@ -0,0 +1,6 @@
+
+let api1 = 'https://api.unsplash.com/search/photos?query=spring&per_page=30&orientation=landscape&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo';
+let api2 = 'https://api.unsplash.com/search/photos?query=spring&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo';
+
+console.log(api1);
+console.log(api2);
